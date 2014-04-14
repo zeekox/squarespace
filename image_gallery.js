64e4ea3396bd4a2869895fd195c5b1959057da56
Mauro Carabotti <carabotti@puzzle.ch>
2014-04-14 20:22:32 +0200

    new css and js to add download fullsize link in gallery

--- image_gallery.js
+++ image_gallery.js
@@ -1,18 +1,14 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 
 <script>
-  jQuery( document ).ready(function($) { 
-    var link_class = 'img-full-size-link';
-    var link_selector = '.' + link_class;
-    $( "div.slide" ).hover(
-      function() {
-       var link = $(this).find("a").first().attr("href") + '?format=original';
-        if ($(this).find(link_selector).length == 0){
-			$(this).append($('<a class="'+link_class+'" onclick="window.location=\''+link+'\'">Download</a>'));
-        }
-      }, function() {
-        $( this ).parent().find(link_selector).remove();
-      }
-    );
-  });
+jQuery( document ).ready(function($) { 
+	$( "div.slide" ).each(
+		function(e) {
+			var link = $(this).find("a").first().attr("href") + '?format=original';
+			$(this).append($('<a class="img-full-size-link" onclick="window.location=\''+link+'\'">Download</a>'));
+	});
+});
 </script>
+
+
+
